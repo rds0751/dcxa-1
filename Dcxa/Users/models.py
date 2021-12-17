ccbbb61cd7052439f388233a04fa14a0a7c9bedc
Swastik8750 <Swastik8750@gmail.com>
2021-12-17 12:31:43 +0530

    kyc model

--- Dcxa/Users/models.py
+++ Dcxa/Users/models.py
@@ -1,2 +1,17 @@
 from django.db import models
 
+class KYC(models.models):
+    username = models.ForeignKey(profile, on_delete=models.CASCADE)
+    name = models.CharField(max_length = 255)
+    photo = models.FileField(upload_to='photo/')
+    livephoto = models.FileField(upload_to='livephoto')
+    documentname = models.CharField(max_length= 255)
+    documentid = models.CharField(max_length=255)
+    documentfront = models.FileField(upload_to='documentfront')
+    documentback = models.FileField(upload_to='documentback')
+    
+
+
+
+
+    
