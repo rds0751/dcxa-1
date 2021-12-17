17d62d85ad2d68a4e200ded6d7ffb16e27b963aa
nishantrajputnr <nishunishuraj18@gmail.com>
2021-12-17 12:22:49 +0530

    profile_model

--- Dcxa/Users/models.py
+++ Dcxa/Users/models.py
@@ -1,2 +1,18 @@
 from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
+from datetime import datetime
 
+
+class profile(models.models):
+    username = models.CharField(max_length=25, primary_key=True)
+    full_name = models.CharField(max_length=255)
+    mobile = PhoneNumberField(unique=False)
+    address = models.TextField(blank=True, null=True)
+    city = models.CharField(max_length=255, blank=True, null=True)
+    state = models.CharField(max_length=255, blank=True, null=True)
+    is_superuser = models.BooleanField(blank=True, default=False)
+    profile_pic = models.FileField(upload_to='profile_pics/', null=True)
+    referral = models.CharField(max_length=25, blank=True, null=True)
+    email = models.EmailField(null=True, blank=True)
+    card_number = models.IntegerField()
+    
